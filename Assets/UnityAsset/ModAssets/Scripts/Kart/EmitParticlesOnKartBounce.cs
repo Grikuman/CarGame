be89817ae7808a0aa95bd9840727e66fbba63e33
ShunkiShimizu <shun.ki0088@gmail.com>
2025-09-26 16:15:20 +0900

    Revert "必要ないアセットを削除"
    This reverts commit 5cc7f637fcc1a9882d13e6bf27cbb443d9d1d750.

--- Assets/UnityAsset/ModAssets/Scripts/Kart/EmitParticlesOnKartBounce.cs
+++ Assets/UnityAsset/ModAssets/Scripts/Kart/EmitParticlesOnKartBounce.cs
@@ -1,26 +1,26 @@
-﻿
+﻿using KartGame.KartSystems;
 using UnityEngine;
 using UnityEngine.Assertions;
 
 [RequireComponent(typeof(ParticleSystem))]
 public class EmitParticlesOnKartBounce : MonoBehaviour 
 {
-    //ParticleSystem effect;
-    //KartBounce vehicleBounce;
+    ParticleSystem effect;
+    KartBounce vehicleBounce;
 
-    //void Awake() 
-    //{
-    //    effect = GetComponent<ParticleSystem>();
-    //    vehicleBounce = GetComponentInParent<KartBounce>();
+    void Awake() 
+    {
+        effect = GetComponent<ParticleSystem>();
+        vehicleBounce = GetComponentInParent<KartBounce>();
 
-    //    Assert.IsNotNull(vehicleBounce, "This particle should be a child of a VehicleBounce!");
-    //}
+        Assert.IsNotNull(vehicleBounce, "This particle should be a child of a VehicleBounce!");
+    }
 
-    //void Update()
-    //{
-    //    if (vehicleBounce.BounceFlag)
-    //    {
-    //        effect.Play();
-    //    }
-    //}
+    void Update()
+    {
+        if (vehicleBounce.BounceFlag)
+        {
+            effect.Play();
+        }
+    }
 }
