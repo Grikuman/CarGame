27e7109f7370ec1f3b4cbb55ef1dd3cdc22918f9
RIKUTO\rikum <rikumanrikumanrikumanrikuman@gmail.com>
2025-10-02 23:26:09 +0900

    マシンのエンジンとプレイヤー入力を作成

---
+++ Assets/Private/Nagadomo/Scripts/MachinePlayerController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MachinePlayerController : MonoBehaviour
+{
+    private MachineEngineController _machineEngineController;
+    private VehiclePhysicsModule _vehiclePhysicsModule;
+    private InputManager _inputManager;
+
+    private void Start()
+    {
+        // コンポーネントを取得する
+        _machineEngineController = GetComponent<MachineEngineController>();
+        _vehiclePhysicsModule = GetComponent<VehiclePhysicsModule>();
+
+        // インプットマネージャーのインスタンスを取得・初期化
+        _inputManager = InputManager.Instance;
+        _inputManager.Init();
+    }
+
+    private void Update()
+    {
+        // 入力値を更新する
+        _inputManager.UpdateDrivingInputAxis();
+        // 入力値を取得する
+        var input = _inputManager.GetCurrentDeviceGamePlayInputSnapshot();
+
+        // ハンドルの更新
+        _vehiclePhysicsModule._input = -input.Handle;
+        // アクセルの更新
+        _machineEngineController.InputThrottle = input.Accelerator;
+        Debug.Log(input.Accelerator);
+    }
+}
