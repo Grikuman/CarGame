f6601ee3530e42d48cb9c202bf8b1c73fd86e390
ShunkiShimizu <shun.ki0088@gmail.com>
2025-10-21 11:04:39 +0900

    変更の破棄

--- Assets/Private/Nagadomo/Scripts/MachinePlayerController.cs
+++ Assets/Private/Nagadomo/Scripts/MachinePlayerController.cs
@@ -4,13 +4,14 @@ using UnityEngine.InputSystem;
 public class MachinePlayerController : MonoBehaviour
 {
     private MachineEngineController _machineEngineController;
-
+    private VehiclePhysicsModule _vehiclePhysicsModule;
     private InputManager _inputManager;
 
     private void Start()
     {
         // コンポーネントを取得する
         _machineEngineController = GetComponent<MachineEngineController>();
+        _vehiclePhysicsModule = GetComponent<VehiclePhysicsModule>();
 
         // インプットマネージャーのインスタンスを取得・初期化
         _inputManager = InputManager.Instance;
@@ -24,11 +25,13 @@ public class MachinePlayerController : MonoBehaviour
         // 入力値を取得する
         var input = _inputManager.GetCurrentDeviceGamePlayInputSnapshot();
 
+        // ハンドルの更新
+        _vehiclePhysicsModule._input = input.Handle * 0.45f;
         // アクセルの更新
         _machineEngineController.InputThrottle = input.Accelerator;
         // ブレーキの更新
         _machineEngineController.InputBrake = input.Brake;
-        
+
         // テスト用
         float inputHorizontal = Input.GetAxis("Horizontal");
         // 横滑りの追加
