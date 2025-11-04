983b435c103cbb24751a886878f2cb8a149e804d
Grikuman <150204154+Grikuman@users.noreply.github.com>
2025-11-04 12:45:47 +0900

    エンジンモジュールデータを修正

--- Assets/Private/Nagadomo/Scripts/Machine/Engine/MachineEngineModuleData.cs
+++ Assets/Private/Nagadomo/Scripts/Machine/Engine/MachineEngineModuleData.cs
@@ -15,11 +15,6 @@ public class MachineEngineModuleData : VehicleModuleFactoryBase
     [Header("質量の設定")]
     [SerializeField] private float _mass; // マシンの質量
 
-    [Header("見た目用設定")]
-    [SerializeField] private float _visualYawAngle;    // 回転時のモデルの最大傾き角度(Yaw)
-    [SerializeField] private float _visualRollAngle;   // 回転時のモデルの最大傾き角度(Roll)
-    [SerializeField] private float _visualRotateSpeed; // 回転補間速度
-
     // 読み取り専用
     public float MaxThrust => _maxThrust;
     public float MaxSpeed => _maxSpeed;
@@ -27,9 +22,6 @@ public class MachineEngineModuleData : VehicleModuleFactoryBase
     public float DragCoeff => _dragCoeff;
     public float BrakingDrag => _brakingDrag;
     public float Mass => _mass;
-    public float VisualYawAngle => _visualYawAngle;
-    public float VisualRollAngle => _visualRollAngle;
-    public float VisualRotateSpeed => _visualRotateSpeed;
 
 
     /// <summary> モジュールを作成する </summary>
