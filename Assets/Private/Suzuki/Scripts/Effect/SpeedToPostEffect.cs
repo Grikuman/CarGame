e40eac0a709ad9c4fbc36698bd151014c55ce98c
masasifuriku <masasifuriku2000@gmail.com>
2025-11-21 14:50:27 +0900

    コースモデルの更新、スピードラインエフェクト追加

---
+++ Assets/Private/Suzuki/Scripts/Effect/SpeedToPostEffect.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class SpeedToPostEffect : MonoBehaviour
+{
+    public Material postEffectMaterial;  // FullScreenShaderGraph のマテリアル
+    public Rigidbody machineRigidbody;              // マシン制御スクリプト
+
+    void Update()
+    {
+        float speed = machineRigidbody.linearVelocity.magnitude; // 速度を取得
+
+        //スピードを正規化して滑らかに
+        float raw = Mathf.Clamp01(speed / 200f);
+        float speedFactor = Mathf.SmoothStep(0f, 1f, raw);
+
+        //シェーダーに渡す
+        postEffectMaterial.SetFloat("_MachineSpeed", speedFactor);
+    }
+}
