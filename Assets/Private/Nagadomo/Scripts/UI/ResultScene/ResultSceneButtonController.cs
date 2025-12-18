7afee25ec2736f544d1e13f41a4335d2019d9428
Grikuman <150204154+Grikuman@users.noreply.github.com>
2025-12-18 15:57:09 +0900

    仮ソロリザルトシーン作成

---
+++ Assets/Private/Nagadomo/Scripts/UI/ResultScene/ResultSceneButtonController.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+
+public class ResultSceneButtonController : MonoBehaviour
+{
+    public static readonly Vector3[] SelectorPositions = new Vector3[]
+    {
+        new Vector3(449.0f, -237.0f, 0),
+        new Vector3(449.0f, -353.0f, 0)
+    };
+
+    [SerializeField] private ButtonBase[] _buttonBases;
+    [SerializeField] private RectTransform _rectTransform;
+
+
+    // 現在の選択番号
+    private int _currentIndex = 0;
+    // グリッドセレクター
+    private UIGridSelector _uiGridSelector = null;
+
+    private void Awake()
+    {
+        _uiGridSelector = new UIGridSelector(2, 1);
+
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // 現在の番号を取得
+        _currentIndex = _uiGridSelector._currentIndex;
+
+        for (int i = 0; i < _buttonBases.Length; i++)
+        {
+            // 選択中のボタンをオンにする
+            if (i == _currentIndex)
+                _buttonBases[i].ChangeAnimationState(_buttonBases[i].GetAnimationState<OnResultSceneButtonAnimationState>());
+            else
+                _buttonBases[i].ChangeAnimationState(_buttonBases[i].GetAnimationState<OffResultSceneButtonAnimationState>());
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _uiGridSelector.Update();
+
+        // ボタン番号が変更された場合
+        if (_currentIndex != _uiGridSelector._currentIndex)
+        {
+            // 番号の更新
+            _currentIndex = _uiGridSelector._currentIndex;
+
+
+            // アニメーションの変更
+            for (int i = 0; i < _buttonBases.Length; i++)
+            {
+                // 選択中のボタンをオンにする
+                if (i == _currentIndex)
+                {
+                    _buttonBases[i].ChangeAnimationState(_buttonBases[i].GetAnimationState<OnResultSceneButtonAnimationState>());
+                    // セレクターの位置変更
+                    _rectTransform.localPosition = SelectorPositions[i];
+                }
+                else
+                {
+                    _buttonBases[i].ChangeAnimationState(_buttonBases[i].GetAnimationState<OffResultSceneButtonAnimationState>());
+                }
+            }
+        }
+        // 決定入力
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _buttonBases[_currentIndex].OnEvent();
+        }
+    }
+}
